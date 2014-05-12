870397b1b468ab9c258ce08bf03267a989d43dd4
piyush123 <piyush@onor.net>
2014-05-11 21:58:07 -0700

    Initial Commit

---
+++ TipCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TipCalculator
+//
+//  Created by piyush shah on 5/11/14.
+//  Copyright (c) 2014 onor inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "tipCalculatorAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([tipCalculatorAppDelegate class]));
+    }
+}
---
+++ TipCalculator/tipCalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  tipCalculatorAppDelegate.h
+//  TipCalculator
+//
+//  Created by piyush shah on 5/11/14.
+//  Copyright (c) 2014 onor inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tipCalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
